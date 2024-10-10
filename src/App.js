53ff7c2d04ab7ecb79e451f5c2723502ca27717a
Parth Bhuva <parth.d.bhuva2331@gmail.com>
2024-10-11 02:44:52 +0530

    add quiz and show result

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import English from "./components/English";
+import Reactjs from "./components/Reactjs";
 
 function App() {
   return (
-    <>
-      <div>App</div>
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/english" element={<English />} />
+        <Route path="/react" element={<Reactjs />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Subjects } from "../components/Subjects";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Subjects />
+    </div>
+  );
+}
